07b41f201d4794dec0851f0e043174f04815140b
chendelin1982 <27513732@qq.com>
2022-10-19 15:04:06 +0800

    commit activemq for example

---
+++ apps/activemq/src/get_version.sh
@@ -0,0 +1,6 @@
+activemqkind=$(docker image list|grep ghcr)
+if [[ $activemqkind == "" ]] ; then
+  sudo echo "activemq version:" $(docker exec -it $1  find /opt/activemq -name activemq-all* | cut -d- -f3) | sudo tee -a /data/logs/install_version.txt
+else
+  sudo echo "activemq version:" $(cat /data/apps/activemq/.env |grep APP_ARTEMIS_VERSION |cut -d= -f2) | sudo tee -a /data/logs/install_version.txt
+fi 
