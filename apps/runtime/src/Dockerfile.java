1c9d054c8c32e66a5efa00284bedd3bec3fe3e80
qiaofeng1227 <76487013@qq.com>
2022-10-24 17:45:30 +0800

    Update Dockerfile.java

--- apps/runtime/src/Dockerfile.java
+++ apps/runtime/src/Dockerfile.java
@@ -1,7 +1,16 @@
-ARG PHP_VERSION=${JAVA_VERSION}
+ARG JDK_VERSION=${JDK_VERSION}
 
-FROM openjdk:${JAVA_VERSION}
+FROM openjdk:${JDK_VERSION}
 
 LABEL maintainer="help@websoft9.com"
-LABEL version="${JAVA_VERSION}"
-LABEL description="JAVA runtime for ${JAVA_VERSION}"
+LABEL version="${JDK_VERSION}"
+LABEL description="JAVA runtime for ${JDK_VERSION}"
+  
+RUN apt-get update && apt-get install -y --no-install-recommends lsb-release  wget openssl git
+
+RUN mkdir -p /data/apps;
+
+COPY --from=tomcat:${TOMCAT_VERSION} /usr/local/tomcat /data/apps
+
+# install supervisord
+RUN apt install -y supervisor
