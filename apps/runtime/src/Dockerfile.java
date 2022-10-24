70f241eb4b8c8b9b1fdade5116b81ae6101005d7
qiaofeng1227 <76487013@qq.com>
2022-10-24 17:37:25 +0800

    Update Dockerfile.java

--- apps/runtime/src/Dockerfile.java
+++ apps/runtime/src/Dockerfile.java
@@ -1,1 +1,7 @@
+ARG PHP_VERSION=${JAVA_VERSION}
 
+FROM openjdk:${JAVA_VERSION}
+
+LABEL maintainer="help@websoft9.com"
+LABEL version="${JAVA_VERSION}"
+LABEL description="JAVA runtime for ${JAVA_VERSION}"
