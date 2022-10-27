b925cdd05852edd5400d1e11583df94a2cfc1463
qiaofeng1227 <76487013@qq.com>
2022-10-27 11:19:07 +0800

    Update nginx_init.sh

--- apps/runtime/src/nginx/nginx_init.sh
+++ apps/runtime/src/nginx/nginx_init.sh
@@ -3,7 +3,7 @@ sudo chmod +x /data/apps/runtime/src/nginx/gitip.sh
 public_ip=bash /data/apps/runtime/src/nginx/gitip.sh
 
 # add nginx.conf
-sudo sed -i "s/$example.domain.com/$public_ip/g" /data/apps/runtime/src/nginx/1.conf
+sudo sed -i "s/example.domain.com/$public_ip/g" /data/apps/runtime/src/nginx/1.conf
 cp /data/apps/runtime/src/nginx/1.conf /var/lib/docker/volumes/runtime_nginx_data/_data/nginx/proxy_host
 
 cd /data/apps/runtime/src/nginx && echo "update proxy_host set domain_names='[\"$public_ip\"]';" | sqlite3 database.sqlite
