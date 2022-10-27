a98e39bf4b901ffc5bf0b418994e4a47010a6fea
qiaofeng1227 <76487013@qq.com>
2022-10-27 09:05:06 +0800

    Update nginx_init.sh

--- apps/runtime/src/nginx/nginx_init.sh
+++ apps/runtime/src/nginx/nginx_init.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
 
+# add nginx.conf
+cp /data/apps/runtime/src/nginx/1.conf /var/lib/docker/volumes/runtime_nginx_data/_data/nginx/proxy_host
+
+cd /data/apps/runtime/src/nginx && echo sqlite3 database.sqlite
