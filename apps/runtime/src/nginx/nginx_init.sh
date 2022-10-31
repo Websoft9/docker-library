48bcd3269232c3a8a4348b1a492a44bbde88f057
qiaofeng1227 <76487013@qq.com>
2022-10-31 14:05:08 +0800

    Update nginx_init.sh

--- apps/runtime/src/nginx/nginx_init.sh
+++ apps/runtime/src/nginx/nginx_init.sh
@@ -16,4 +16,4 @@ docker restart nginx-proxy-manager
 echo "nginx_proxy_port: 9001" >> /credentials/password.txt
 echo "nginx_proxy_user: admin@example.com" >> /credentials/password.txt
 echo "nginx_proxy_password: changeme" >> /credentials/password.txt
-docker stop java8 java11 java13 java14 java15 java18 java 19
+docker stop java8 java11 java13 java14 java15 java18 java19
