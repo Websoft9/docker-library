1b0b5d5fab86e7ddba1d0952c5e5c6cfae3ea1fd
qiaofeng1227 <76487013@qq.com>
2022-11-07 12:26:41 +0800

    Update nginx_init.sh

--- apps/runtime/src/nginx/nginx_init.sh
+++ apps/runtime/src/nginx/nginx_init.sh
@@ -5,7 +5,7 @@ public_ip=`wget -O - https://download.websoft9.com/ansible/get_ip.sh | bash`
 sudo sed -i "s/example.domain.com/$public_ip/g" /data/apps/runtime/src/nginx/cockpit-proxy.conf
 cp /data/apps/runtime/src/nginx/cockpit-proxy.conf /var/lib/docker/volumes/runtime_nginx_data/_data/nginx/proxy_host/websoft9.conf
 
-docker restart nginx-proxy-manager
+docker restart nginxproxymanager
 
 echo "nginx_proxy_port: 9001" >> /credentials/password.txt
 echo "nginx_proxy_user: admin@example.com" >> /credentials/password.txt
