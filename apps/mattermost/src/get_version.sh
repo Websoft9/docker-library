b7182082dd73346d9ba686026a143f30309975b1
qiaofeng1227 <76487013@qq.com>
2022-11-10 16:17:53 +0800

    Update get_version.sh

--- apps/mattermost/src/get_version.sh
+++ apps/mattermost/src/get_version.sh
@@ -1,1 +1,1 @@
-sudo echo "Mattermost version:" $(docker exec -i $1 cat /bitnami/mattermost/bin/mattermost version |grep Version  |head -1 |cut -d: -f2)" 1>> /data/logs/install_version.txt
+sudo echo "Mattermost version:" $(docker exec -i mattermost /mattermost/bin/mattermost version)" 1>> /data/logs/install_version.txt
