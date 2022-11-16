2d9d16d57b54773b994fd2b6978b1f16ca8eda6b
徐月婷 <93903193+xyt52@users.noreply.github.com>
2022-11-16 17:23:56 +0800

    Update get_version.sh

--- apps/graylog/src/get_version.sh
+++ apps/graylog/src/get_version.sh
@@ -1,1 +1,2 @@
-#sudo echo "graylog version:" $(docker image inspect graylog/graylog:4.3 | jq .[].Config.Labels | grep "org.label-schema.version" | cut -d ":" -f 2 | sed 's/\"//g')  1>> /data/logs/install_version.txt
+#sudo echo "graylog version:" $(docker image inspect graylog/graylog:4.3 | jq .[].Config.Labels | grep "org.label-schema.version" | cut -d ":" -f 2 | sed 's/\"//g') |sudo tee -a /data/logs/install_version.txt
+
