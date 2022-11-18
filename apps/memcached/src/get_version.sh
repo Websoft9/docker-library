b04276104a16caf19b31adf0e4f6e084856e07ce
徐月婷 <93903193+xyt52@users.noreply.github.com>
2022-11-18 10:13:46 +0800

    Update get_version.sh

--- apps/memcached/src/get_version.sh
+++ apps/memcached/src/get_version.sh
@@ -1,1 +1,1 @@
-sudo echo "memcached:" $(docker inspect memcached:$(docker images | grep memcached |awk '{print $2}') |grep -m 1 "MEMCACHED_VERSION"|cut -d'=' -f2|cut -d"\"" -f1) |sudo tee -a /data/logs/install_version.txt
+sudo echo "memcached version:" $(docker inspect memcached:$(docker images | grep memcached |awk '{print $2}') |grep -m 1 "MEMCACHED_VERSION"|cut -d'=' -f2|cut -d"\"" -f1) |sudo tee -a /data/logs/install_version.txt
