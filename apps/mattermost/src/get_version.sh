efd311a3ec2c39be153ee8c370cfced4e4c4ebfb
徐月婷 <93903193+xyt52@users.noreply.github.com>
2022-11-18 12:48:39 +0800

    Update get_version.sh

--- apps/mattermost/src/get_version.sh
+++ apps/mattermost/src/get_version.sh
@@ -1,1 +1,1 @@
-sudo echo "Mattermost version:" $(docker exec -i mattermost /mattermost/bin/mattermost version)" 1>> /data/logs/install_version.txt
+sudo echo "Mattermost version:" $(sudo docker exec -i mattermost /mattermost/bin/mattermost version|grep  -i "version" | cut -d ":" -f 2) |sudo tee -a /data/logs/install_version.txt
