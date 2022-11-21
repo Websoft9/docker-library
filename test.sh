b822f09c53d1f73378b39fd424766f71b3e46d79
chendelin1982 <27513732@qq.com>
2022-11-21 11:07:16 +0800

    rollback

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ files=$(ls apps/)
 for file in $files
 do
   if [ -f apps/$file/docker-compose.yml ];then
-    sed -i 's/restart: always/restart: unless-stopped/g' apps/$file/docker-compose.yml
+    sed -i 's/restart: unless-stopped/restart: always/g' apps/$file/docker-compose.yml
   else 
     echo "apps/$file/docker-compose.yml dosen't exist"
   fi
