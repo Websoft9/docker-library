a7f92695c98ce826b25f3620264f0dca4b89ff31
chendelin1982 <27513732@qq.com>
2022-11-21 10:48:45 +0800

    commit

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+files=$(ls apps/)
+for file in $files
+do
+  if [ -f apps/$file/docker-compose.yml ];then
+    sed -i 's/restart: always/restart: unless-stopped/g' apps/$file/docker-compose.yml
+  else 
+    echo "apps/$file/docker-compose.yml dosen't exist"
+  fi
+done
