f776d6b0307dcf077bb01d8ea56f244fa2ca1b59
qiaofeng1227 <76487013@qq.com>
2022-11-23 22:17:44 +0800

    Update after_up.sh

--- apps/wordpress/src/after_up.sh
+++ apps/wordpress/src/after_up.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sleep 10s
 
-sudo docker cp wp-cli.phar wordpress:/tmp
+sudo docker cp /data/apps/wordpress/data/backup/wp-cli.phar wordpress:/tmp
 sudo docker exec -it wordpress chmod +x /tmp/wp-cli.phar
 sudo docker exec -it wordpress mv /tmp/wp-cli.phar /usr/local/bin/wp
