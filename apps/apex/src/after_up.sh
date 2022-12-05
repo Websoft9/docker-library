09ccbd72cfed0632edcd97ac0855a678f914cdc6
qiaofeng1227 <76487013@qq.com>
2022-12-05 09:59:16 +0800

    Update after_up.sh

--- apps/apex/src/after_up.sh
+++ apps/apex/src/after_up.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
-while ( ! [ -n $(docker ps -q -f "name=^apex$") ] )
+status="000"
+while [ $status == "000" ]
 do
-        echo "starting..."
-        sleep 2
+        echo 'starting.....'
+        status=$(curl -IL -m 5 -s -w "%{http_code}\n" -o /dev/null "localhost:9001")
+        sleep 15
 done
-
-sudo docker exec -it apex /bin/bash -c "
-dirpath="/opt/oracle/apex/*/builder/zh-cn"
-filename="/opt/oracle/sqlcl/bin/sql"
-while ( [ ! -d "$dirpath" ] && [ ! -e "$filename" ] )
-do
-        echo "starting......"
-        sleep 2
-done"
+echo "$status"
 echo "startup complete"
 export password=$(grep "apex_db_oracle_password" /credentials/password.txt|awk -F ": " '{print $2}')
 echo "begin to load chinese..."
-sudo docker exec -it apex /bin/bash -c "      
+sudo docker exec -it apex /bin/bash -c "
 cd /opt/oracle/apex/*/builder/zh-cn
-NLS_LANG=Chinese_Chinese.AL32UTF8 
+NLS_LANG=Chinese_Chinese.AL32UTF8
 echo exit | /opt/oracle/sqlcl/bin/sql sys/"$password"@apex-db:1521/xepdb1 as sysdba @load_zh-cn.sql
 "
 echo "finished"
