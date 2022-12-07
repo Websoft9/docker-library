81dd75d2c2ad1c546206af64c8c153d7a05eadd5
童颖佳 <81067792+Tooy1011@users.noreply.github.com>
2022-12-07 11:33:33 +0800

    Update after_up.sh

--- apps/apex/src/after_up.sh
+++ apps/apex/src/after_up.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 status="000"
 echo "starting......" >> /tmp/debug.txt
+sudo sh -c 'echo "init-password started at" $(date -d now)  1>> /tmp/debug.txt'
 while [ $status == "000" ]
 do
         status=$(curl -IL -m 5 -s -w "%{http_code}\n" -o /dev/null "localhost:9001")
-        sleep 15
+        sleep 20
 done
+sudo sh -c 'echo "init-password started at" $(date -d now)  1>> /tmp/debug.txt'
 echo "$status" >> /tmp/debug.txt
 echo "startup complete" >> /tmp/debug.txt
-echo "getting password..." >> /tmp/debug.txt
-echo $1 >> /tmp/password.txt
-docker cp /tmp/password.txt apex:/tmp/password.txt
+sleep 10
 echo "begin to load chinese..." >> /tmp/debug.txt
+export $1
 sudo docker exec -it apex /bin/bash -c "
-password=$(cat /tmp/password.txt)      
+echo $1 >> /tmp/debug.txt
 cd /opt/oracle/apex/*/builder/zh-cn
 NLS_LANG=Chinese_Chinese.AL32UTF8 
-echo exit | /opt/oracle/sqlcl/bin/sql sys/"$password"@apex-db:1521/xepdb1 as sysdba @load_zh-cn.sql >> /tmp/debug.txt
+echo exit | /opt/oracle/sqlcl/bin/sql sys/"$1"@apex-db:1521/xepdb1 as sysdba @load_zh-cn.sql >> /tmp/debug.txt
 "
 echo "finished" >> /tmp/debug.txt
