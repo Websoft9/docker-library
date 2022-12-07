26b39bb36f46f9d7c6a2ca37e8c9f67793152451
童颖佳 <81067792+Tooy1011@users.noreply.github.com>
2022-12-07 17:58:22 +0800

    Update after_up.sh

--- apps/apex/src/after_up.sh
+++ apps/apex/src/after_up.sh
@@ -14,11 +14,11 @@ echo "$status" >> /tmp/debug.txt
 echo "startup complete" >> /tmp/debug.txt
 echo "begin to load chinese..." >> /tmp/debug.txt
 password=$1
-echo $password
+echo $password >> /tmp/debug.txt
 export password
 sudo sh -c 'echo "started at" $(date -d now)  1>> /tmp/debug.txt'
 sudo docker exec -it apex /bin/bash -c "
 cd /opt/oracle/apex/*/builder/zh-cn && NLS_LANG=Chinese_Chinese.AL32UTF8 && echo exit | /opt/oracle/sqlcl/bin/sql sys/"$password"@apex-db:1521/xepdb1 as sysdba @load_zh-cn.sql
-"
+" >> /tmp/debug.txt
 sudo sh -c 'echo "ended at" $(date -d now)  1>> /tmp/debug.txt'
 echo "finished" >> /tmp/debug.txt
