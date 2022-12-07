fe4a339f5d86e46388b22b310bc00a0b01164704
徐月婷 <93903193+xyt52@users.noreply.github.com>
2022-12-07 11:38:51 +0800

    Update get_version.sh

--- apps/erpnext/src/get_version.sh
+++ apps/erpnext/src/get_version.sh
@@ -1,1 +1,2 @@
-sudo echo "erpnext version:" $(cat /data/apps/erpnext/.env |grep  "APP_VERSION" |awk -F"=" '{print $2}') 1>> /data/logs/install_version.txt
+sudo echo "erpnext version:" $(docker exec -i erpnext cat /home/frappe/frappe-bench/apps/erpnext/erpnext/__init__.py|grep version |cut -d "=" -f2
+) 1>> /data/logs/install_version.txt
