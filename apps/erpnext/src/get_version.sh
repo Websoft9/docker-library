e26617a24a73311bd8a49537184e584fc5489926
qiaofeng1227 <76487013@qq.com>
2022-12-07 12:10:23 +0800

    Update get_version.sh

--- apps/erpnext/src/get_version.sh
+++ apps/erpnext/src/get_version.sh
@@ -1,2 +1,1 @@
-sudo echo "erpnext version:" $(docker exec -i erpnext cat /home/frappe/frappe-bench/apps/erpnext/erpnext/__init__.py|grep version |cut -d "=" -f2
-) 1>> /data/logs/install_version.txt
+sudo echo "erpnext version:" $(docker exec -i erpnext cat /home/frappe/frappe-bench/apps/erpnext/erpnext/__init__.py|grep version |cut -d "=" -f2) 1>> /data/logs/install_version.txt
