6e0a91a1b8c555efaab866915c13bb89063b308e
徐月婷 <93903193+xyt52@users.noreply.github.com>
2022-12-21 15:04:44 +0800

    Update cmd.sh

--- apps/zentao/cmd.sh
+++ apps/zentao/cmd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# customrized cmd powered by Websoft9
+# customrized cmd powered by Websoft
 
 sed -i "s/dbHost : '127.0.0.1'/dbHost : '$MYSQL_HOST'/g" /var/www/zentaopms/module/install/control.php
 sed -i "s/dbName : 'zentao'/dbName : '$MYSQL_DB'/g" /var/www/zentaopms/module/install/control.php
