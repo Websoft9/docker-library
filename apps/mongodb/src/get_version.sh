7c4e64560b4b0ccdf41de7de1fe0743922c24d6b
张露 <62973206+zl-go@users.noreply.github.com>
2022-12-29 14:04:37 +0800

    Update get_version.sh

--- apps/mongodb/src/get_version.sh
+++ apps/mongodb/src/get_version.sh
@@ -1,1 +1,1 @@
-sudo echo "MongoDB version:" $(docker exec -i $1 mongod --version | grep db) 1>> /data/logs/install_version.txt
+sudo echo "MongoDB version:" $(docker exec -i $1 mongod --version | grep db | cut -d " " -f 3) >> /data/logs/install_version.txt
