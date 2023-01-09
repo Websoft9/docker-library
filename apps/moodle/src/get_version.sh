574d0667b838db784aaa6c61eb63a67703fd448a
徐月婷 <93903193+xyt52@users.noreply.github.com>
2023-01-09 15:15:37 +0800

    Update get_version.sh

--- apps/moodle/src/get_version.sh
+++ apps/moodle/src/get_version.sh
@@ -1,1 +1,1 @@
-sudo echo "moodle version": $(docker exec -i $1 cat /bitnami/moodle/version.php  | grep "\$release" | awk '{print $3}' | sed 's/^.//') | tee -a /data/logs/install_version.txt
+sudo echo "moodle version": $(docker exec -i moodle cat /bitnami/moodle/licenses/gpl-source-links.txt | cut -d "," -f 1 | cut -d "-" -f 2) | tee -a /data/logs/install_version.txt
