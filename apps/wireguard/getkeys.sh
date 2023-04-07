cc87a16913f83d8f2e2530ba074646052358bfc6
Darren <27513732@qq.com>
2023-04-07 16:33:12 +0800

    Update getkeys.sh

--- apps/wireguard/getkeys.sh
+++ apps/wireguard/getkeys.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
 
+
+#--------------------
+# Set you port
+#--------------------
+
+
+# Set the filename
+filename=".env"
+
+# Read the value of APP_UDP_PORT from the file
+port=$(grep -oP 'APP_UDP_PORT=\K\d+' "$filename")
+
+# Increment the value by 1
+new_port=$((port + 1))
+
+# Replace the old value with the new value in the file
+sed -i "s/APP_UDP_PORT=$port/APP_UDP_PORT=$new_port/" "$filename"
+
+cat .env
+sudo docker down -v
+sudo docker compose up -d
+sleep 10s
+
 #--------------------------------------------
 # Copy keys to data directory
 #--------------------------------------------
