222ad5d40ad306121661167f5e8685e1756d0f15
qiaofeng1227 <76487013@qq.com>
2023-04-08 12:22:53 +0800

    Update get_version.sh

--- apps/apitable/src/get_version.sh
+++ apps/apitable/src/get_version.sh
@@ -1,1 +1,1 @@
-sudo echo "apitable" $(docker exec -it apitable-web-server-1 cat package.json|grep version) 1>> /data/logs/install_version.txt
+sudo echo "apitable" $(docker exec -it apitable-webserver sed -n '3p' package.json) 1>> /data/logs/install_version.txt
