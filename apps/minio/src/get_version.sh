9f8bcff628c575a572d8e8e0811bfd7ae8916c4c
qiaofeng1227 <76487013@qq.com>
2023-04-08 12:07:24 +0800

    Update get_version.sh

--- apps/minio/src/get_version.sh
+++ apps/minio/src/get_version.sh
@@ -1,1 +1,1 @@
-sudo echo "minio version:" $(docker inspect minio | grep '"version":'|cut -d ':' -f 2) |sudo tee -a /data/logs/install_version.txt
+sudo echo "minio version:" $(docker inspect $1 | grep '"version":'|cut -d ':' -f 2) |sudo tee -a /data/logs/install_version.txt
