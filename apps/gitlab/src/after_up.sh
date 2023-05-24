6a1451a856c2395b86302499bb3126cd016e4ff4
qiaofeng1227 <76487013@qq.com>
2023-05-24 09:37:29 +0800

    password

--- apps/gitlab/src/after_up.sh
+++ apps/gitlab/src/after_up.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sleep 60s
 gitlab_pwd=$(sudo grep 'Password:' /data/apps/gitlab/data/gitlab_config/initial_root_password|cut -d: -f2)
-
+echo "POWER_PASSWORD=$gitlab_pwd" >> /data/apps/gitlab/.env
 echo "APP_PASSWORD=$gitlab_pwd" >> /data/apps/gitlab/.env
--- apps/wordpress/src/after_up.sh
+++ apps/wordpress/src/after_up.sh
@@ -1,4 +1,1 @@
-#!/bin/bash
-sleep 10s
 
-sudo docker cp /data/apps/wordpress/data/backup/wp wordpress:/usr/local/bin
