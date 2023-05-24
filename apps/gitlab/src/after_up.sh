8dab538cb9ae378d9c061a12478565a88278eb7d
qiaofeng1227 <76487013@qq.com>
2023-05-24 11:12:23 +0800

    gitlab

--- apps/gitlab/src/after_up.sh
+++ apps/gitlab/src/after_up.sh
@@ -2,7 +2,6 @@
 while [ ! -f /var/lib/docker/volumes/gitlab_gitlab_config/_data/initial_root_password ]
 do
     sleep 3
-    echo "1111" >> /tmp/gitlab
 done 
 gitlab_pwd=$(sudo grep 'Password:' /var/lib/docker/volumes/gitlab_gitlab_config/_data/initial_root_password|cut -d: -f2)
 echo "POWER_PASSWORD=$gitlab_pwd" >> /data/apps/gitlab/.env
