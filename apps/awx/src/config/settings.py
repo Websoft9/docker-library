918640f8f96134eeb55e6bd71633e80a1ef47197
lihb <18910288715@163.com>
2023-07-29 17:35:27 +0800

    🐞 fix: 修复ALLOWED_HOSTS错误

--- apps/awx/src/config/settings.py
+++ apps/awx/src/config/settings.py
@@ -8,7 +8,7 @@ DEBUG = False
 SECRET_KEY = "XzFwTFZnM0NSLUhWMHdybEVPaEQ="
 CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1",
                         f"http://{ip_address}", 'http://127.0.0.1:8052']
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '*']
 AWX_AUTO_DEPROVISION_INSTANCES = True
 SYSTEM_UUID = str(uuid.uuid4())
 
