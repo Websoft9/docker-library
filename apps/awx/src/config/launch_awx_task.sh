e7141bc5a6ad1b6f1a2adc9c804fceecd34215fc
lihb <lihb@lxzwedu.com>
2023-07-31 16:34:45 +0800

    🐞 fix: 修复没有ansible-run的问题

---
+++ apps/awx/src/config/launch_awx_task.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+if [ `id -u` -ge 500 ]; then
+    echo "awx:x:`id -u`:`id -g`:,,,:/var/lib/awx:/bin/bash" >> /tmp/passwd
+    cat /tmp/passwd > /etc/passwd
+    rm /tmp/passwd
+fi
+
+if [ -n "${AWX_KUBE_DEVEL}" ]; then
+    pushd /awx_devel
+    make awx-link
+    popd
+
+    export SDB_NOTIFY_HOST=$MY_POD_IP
+fi
+
+set -e
+
+wait-for-migrations
+
+export PATH=$PATH:/var/lib/awx/venv/awx/bin
+
+# 迁移数据库
+awx-manage migrate
+
+awx-manage create_preload_data
+awx-manage register_default_execution_environments
+# 创建admin账号密码
+if output=$(awx-manage createsuperuser --noinput --username=admin --email=admin@localhost 2> /dev/null); then
+    echo $output
+fi
+echo "Admin password: ${DJANGO_SUPERUSER_PASSWORD}"
+
+awx-manage provision_instance
+awx-manage register_queue --queuename=controlplane --instance_percent=100
+awx-manage register_queue --queuename=default --instance_percent=100
+
+exec supervisord -c /etc/supervisord_task.conf
