53300833b6774a055029467def090afea3292b0a
qiaofeng1227 <76487013@qq.com>
2023-11-28 17:37:58 +0800

    pr

--- i18n/pr.py
+++ i18n/pr.py
@@ -1,6 +1,6 @@
 import json
 import os
 
-# translation.json 以增量的方式合并到 po.json
+# translation.json 以合并替换的方式合并到 po.json
 # https://github.com/Websoft9/plugin-appstore/blob/main/public/po.zh_CN.js
 # https://github.com/Websoft9/plugin-appstore/blob/main/public/po.en_US.js
