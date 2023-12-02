dc50e6184d69942791b15bdc09bb76fd9d44ca31
赵璟 <jing.zhao@websoft9.com>
2023-12-02 17:22:01 +0800

    Update sync_version_to_contentful.py

--- build/sync_version_to_contentful.py
+++ build/sync_version_to_contentful.py
@@ -10,19 +10,19 @@ SPACE_ID = "ffrhttfighww"
 
 # 设置请求头
 headers = {
-  "Authorization": f"Bearer {ACCESS_TOKEN}",
-  "Content-Type": "application/vnd.contentful.management.v1+json"
+    "Authorization": f"Bearer {ACCESS_TOKEN}",
+    "Content-Type": "application/vnd.contentful.management.v1+json"
 }
 
 def update_contentful(product_name):
-  # 获取 Contentful 中的 Product entry
-  response = requests.get(
-      f"{CONTENTFUL_MANAGEMENT_API}/spaces/{SPACE_ID}/environments/master/entries?content_type=product&fields.key={product_name}",
-      headers=headers
-  )
-  response.raise_for_status()
-  entries = response.json()
-  # 打印返回的数据
-  print(json.dumps(entries, indent=2))
+    # 获取 Contentful 中的 Product entry
+    response = requests.get(
+        f"{CONTENTFUL_MANAGEMENT_API}/spaces/{SPACE_ID}/environments/master/entries?content_type=product&fields.key={product_name}",
+        headers=headers
+    )
+    response.raise_for_status()
+    entries = response.json()
+    # 打印返回的数据
+    print(json.dumps(entries, indent=2))
 
 update_contentful("wordpress")
