b9ec3290dc0e867a2689c80a2d324c7266a159db
Darren <27513732@qq.com>
2023-12-06 07:48:14 +0800

    test build_files

--- build/compose_files.py
+++ build/compose_files.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import subprocess
 
 # sys.argv[1] is the first argument passed to the script
 input_arg = sys.argv[1]
@@ -9,4 +11,19 @@ filenames = input_arg.split(',')
 # Remove any empty strings from the list
 filenames = [filename for filename in filenames if filename]
 
-print(filenames)  # This will print the list of filenames
+
+for filename in filenames:
+    # 获取文件的目录和基础名
+    directory, basename = os.path.split(filename)
+
+    # 从基础名中提取 * 部分
+    name_part = basename.split('-')[2].split('.')[0]  # 从 'docker-compose-*.yml' 中提取 '*'
+
+    # 构造新的文件名
+    new_filename = os.path.join(directory, f'docker-compose-{name_part}-merged.yml')
+
+    # 构造 docker-compose 命令
+    cmd = ['docker-compose', '-f', 'docker-compose.yml', '-f', filename, 'config', '>', new_filename]
+
+    # 执行命令
+    subprocess.run(' '.join(cmd), shell=True, check=True)
