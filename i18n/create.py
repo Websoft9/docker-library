f0ab8857565dccb011e140664d34be4d9943bb4b
qiaofeng1227 <76487013@qq.com>
2023-12-05 14:23:12 +0800

    change array to str

--- i18n/create.py
+++ i18n/create.py
@@ -1,6 +1,23 @@
+import glob
 import json
-import os
+from dotenv import dotenv_values
 
-# 从目录应用中获取W9_*_SET，合并去重成为新的 envs 数组
+# 读取已有的 translation.json 文件
+with open('translation.json', 'r') as f:
+    translation = json.load(f)
 
-# 将 envs 数组以增量的方式合并到 translation.json
+# 遍历所有 .env 文件
+for filename in glob.glob('../apps/*/.env'):
+    # 解析 .env 文件
+    env_dict = dotenv_values(filename)
+    # 遍历所有环境变量
+    for key in env_dict.keys():
+        # 如果 key 以 'W9_' 开头并以 '_SET' 结尾
+        if key.startswith('W9_') and key.endswith('_SET'):
+            # 如果 key 不在 translation.json 中，将其添加到 translation.json 中
+            if key not in translation:
+                translation[key] = ["", ""]
+
+# 将修改后的 translation.json 写回到文件中
+with open('translation.json', 'w') as f:
+    json.dump(translation, f, indent=4)
