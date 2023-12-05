ef11716b30459f02874da091448e3d4965927e49
Darren <27513732@qq.com>
2023-12-05 19:32:46 +0800

    test build_files

--- build/compose_files.py
+++ build/compose_files.py
@@ -2,6 +2,7 @@ import sys
 
 def main():
     # sys.argv[0] 是脚本名，sys.argv[1] 是第一个参数
+    print(f'sys.argv: {sys.argv}')  # 打印 sys.argv 的值
     compose_files = sys.argv[1]
 
     print(f'compose_files: {compose_files}')
