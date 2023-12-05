d6af013385937ac31ae7686d7654cf6c7ba6d9f3
Darren <27513732@qq.com>
2023-12-06 07:32:27 +0800

    test build files

--- build/compose_files.py
+++ build/compose_files.py
@@ -1,11 +1,12 @@
 import sys
 
-def main():
-    # sys.argv[0] 是脚本名，sys.argv[1] 是第一个参数
-    print(f'sys.argv: {sys.argv}')  # 打印 sys.argv 的值
-    compose_files = sys.argv[1]
+# sys.argv[1] is the first argument passed to the script
+input_arg = sys.argv[1]
 
-    print(f'compose_files: {compose_files}')
+# Split the input argument on commas to get a list of filenames
+filenames = input_arg.split(',')
 
-if __name__ == '__main__':
-    main()
+# Remove any empty strings from the list
+filenames = [filename for filename in filenames if filename]
+
+print(filenames)  # This will print the list of filenames
