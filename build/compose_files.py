0cadc0fca9749e4b339d5cfea62411d51ce5048f
Darren <27513732@qq.com>
2023-12-06 08:11:08 +0800

    test build files

--- build/compose_files.py
+++ build/compose_files.py
@@ -11,13 +11,13 @@ filenames = input_arg.split(',')
 # Remove any empty strings from the list
 filenames = [filename for filename in filenames if filename]
 
-# Load the base docker-compose file
-with open('docker-compose.yml', 'r') as base_file:
-    base_config = yaml.safe_load(base_file)
-
 for filename in filenames:
     # 获取文件的目录和基础名
     directory, basename = os.path.split(filename)
+
+    # Load the base docker-compose file from the same directory as filename
+    with open(os.path.join(directory, 'docker-compose.yml'), 'r') as base_file:
+        base_config = yaml.safe_load(base_file)
 
     # 从基础名中提取 * 部分
     name_part = basename.split('-')[2].split('.')[0]  # 从 'docker-compose-*.yml' 中提取 '*'
@@ -34,4 +34,4 @@ for filename in filenames:
 
     # Write the merged configuration to the new file
     with open(new_filename, 'w') as output_file:
-        yaml.safe_dump(base_config, output_file)
+        yaml
