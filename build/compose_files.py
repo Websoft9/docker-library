66b508fa87847aab90ac586e3196b96d02208b27
Darren <27513732@qq.com>
2023-12-06 08:08:14 +0800

    test build_files

--- build/compose_files.py
+++ build/compose_files.py
@@ -1,6 +1,6 @@
 import sys
 import os
-import subprocess
+import yaml
 
 # sys.argv[1] is the first argument passed to the script
 input_arg = sys.argv[1]
@@ -11,6 +11,9 @@ filenames = input_arg.split(',')
 # Remove any empty strings from the list
 filenames = [filename for filename in filenames if filename]
 
+# Load the base docker-compose file
+with open('docker-compose.yml', 'r') as base_file:
+    base_config = yaml.safe_load(base_file)
 
 for filename in filenames:
     # 获取文件的目录和基础名
@@ -22,9 +25,13 @@ for filename in filenames:
     # 构造新的文件名
     new_filename = os.path.join(directory, f'{name_part}.yml')
 
-    # 构造 docker-compose 命令
-    docker_compose_file = os.path.join(directory, 'docker-compose.yml')
-    cmd = ['docker-compose', '-f', docker_compose_file, '-f', filename, 'config', '>', new_filename]
+    # Load the overlay file
+    with open(filename, 'r') as overlay_file:
+        overlay_config = yaml.safe_load(overlay_file)
 
-    # 执行命令
-    subprocess.run(' '.join(cmd), shell=True, check=True)
+    # Merge the base and overlay configurations
+    base_config.update(overlay_config)
+
+    # Write the merged configuration to the new file
+    with open(new_filename, 'w') as output_file:
+        yaml.safe_dump(base_config, output_file)
