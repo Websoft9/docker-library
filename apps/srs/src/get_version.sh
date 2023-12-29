037638a66f54220e8a3bacbd574618885c70c8c6
qiaofeng1227 <76487013@qq.com>
2023-12-29 09:39:06 +0800

    src

--- apps/srs/src/get_version.sh
+++ apps/srs/src/get_version.sh
@@ -1,1 +1,1 @@
-sudo echo "srs version: 6.0.45" |sudo  tee -a /data/logs/install_version.txt
+sudo echo "sqlserver version:" $(cat /data/apps/sqlserver/.env |grep -i "W9_VERSION" |awk -F "=" '{print  $2}') |sudo tee -a /data/logs/install_version.txt
