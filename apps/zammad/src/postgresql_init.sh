d940b3ecfe3045a4f66d805287176b14d62f7afd
qiaofeng1227 <76487013@qq.com>
2024-01-10 11:47:21 +0800

    postgresql

---
+++ apps/zammad/src/postgresql_init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# create zammad user and database
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE USER zammad WITH PASSWORD '${ZAMMAD_PASSWORD}';
+    CREATE DATABASE zammad;
+    GRANT ALL PRIVILEGES ON DATABASE zammad TO zammad;
+
+EOSQL
