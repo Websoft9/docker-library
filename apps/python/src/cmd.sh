c12faf2a25bf6f8c012597b7659da4e7f753d159
qiaofeng1227 <76487013@qq.com>
2024-04-09 16:55:45 +0800

    python sample

--- apps/python/src/cmd.sh
+++ apps/python/src/cmd.sh
@@ -1,3 +1,19 @@
 echo "Add your CI code here, below is example"
 
-# add your publish python app
+# pip install fastapi uvicorn[standard]
+# cat << 'EOF' > main.py
+# from typing import Union
+
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
+# EOF
+# uvicorn main:app --host 0.0.0.0 --port 5000
