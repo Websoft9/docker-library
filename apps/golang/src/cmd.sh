0800bccaa4e844897042892fdb70bfa1afe01d9e
qiaofeng1227 <76487013@qq.com>
2024-04-10 16:02:03 +0800

    golang

--- apps/golang/src/cmd.sh
+++ apps/golang/src/cmd.sh
@@ -1,23 +1,23 @@
 echo "Add your CI code here, below is example"
 
-go mod init myapp
-go get -u github.com/gin-gonic/gin
-cat > myapp.go <<EOF
-package main
+# go mod init myapp
+# go get -u github.com/gin-gonic/gin
+# cat > myapp.go <<EOF
+# package main
 
-import (
-  "net/http"
-  "github.com/gin-gonic/gin"
-)
+# import (
+#   "net/http"
+#   "github.com/gin-gonic/gin"
+# )
 
-func main() {
-  r := gin.Default()
-  r.GET("/", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-      "message": "Hello World!",
-    })
-  })
-  r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
-}
-EOF
-go run myapp.go
+# func main() {
+#   r := gin.Default()
+#   r.GET("/", func(c *gin.Context) {
+#     c.JSON(http.StatusOK, gin.H{
+#       "message": "Hello World!",
+#     })
+#   })
+#   r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+# }
+# EOF
+# go run myapp.go
