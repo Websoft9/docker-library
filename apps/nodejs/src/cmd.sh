24ac11a968bed980460656c29865be8241df6a98
qiaofeng1227 <76487013@qq.com>
2024-04-10 15:18:42 +0800

    nodejs

--- apps/nodejs/src/cmd.sh
+++ apps/nodejs/src/cmd.sh
@@ -1,7 +1,17 @@
 echo "Add your CI code here, below is example"
 
-#cd /usr/src/app 
-#npx create-docusaurus@latest classic
-#cd classic
-#yarn install
-#npm run start -- --host 0.0.0.0
+# npm install express --save
+# cat > app.js <<EOF
+# const express = require('express')
+# const app = express()
+# const port = 3000
+
+# app.get('/', (req, res) => {
+#   res.send('Hello World!')
+# })
+
+# app.listen(port, () => {
+#   console.log(\`Example app listening on port \${port}\`)
+# })
+# EOF
+# node app.js
