ee23ed9ca56840d594970d8b0f59421ffa110cd7
qiaofeng1227 <76487013@qq.com>
2024-04-12 13:44:22 +0800

    jetty

--- apps/jetty/src/cmd.sh
+++ apps/jetty/src/cmd.sh
@@ -1,9 +1,6 @@
 ###  This script is running before Jetty starting  ##############
 ###  You can add your CI code here, below is example
 
-### Install os packages
-# yum install wget -y
-
-
-### Install java sample, access by: http://URL/sample
+### Install java sample, access by: http://URL;only support jetty 11
 # cd /var/lib/jetty/webapps
+# curl -o ROOT.war https://tomcat.apache.org/tomcat-11.0-doc/appdev/sample/sample.war
