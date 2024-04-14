6f16786ea4fdf8d4337ded09aea3e2738408bce6
qiaofeng1227 <76487013@qq.com>
2024-04-14 09:18:15 +0800

    Update cmd.sh

--- apps/dotnet/src/cmd.sh
+++ apps/dotnet/src/cmd.sh
@@ -1,6 +1,7 @@
 echo "Add your CI code here, below is example"
 # You should run application at port 8080
 
+### net8.0 for example
 # git clone --depth=1 https://github.com/dotnet/dotnet-docker
 # cd dotnet-docker/samples/aspnetapp/aspnetapp
 # dotnet publish -c Release -o out
