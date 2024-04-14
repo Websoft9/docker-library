f25eacdee428e35e571741e24143fe25b040235c
qiaofeng1227 <76487013@qq.com>
2024-04-14 09:17:39 +0800

    Update cmd.sh

--- apps/dotnet/src/cmd.sh
+++ apps/dotnet/src/cmd.sh
@@ -1,7 +1,7 @@
 echo "Add your CI code here, below is example"
 # You should run application at port 8080
 
-#gem install bundler jekyll
-#jekyll new mysite
-#cd mysite
-#bundle exec jekyll serve --host 0.0.0.0 --port 8080
+# git clone --depth=1 https://github.com/dotnet/dotnet-docker
+# cd dotnet-docker/samples/aspnetapp/aspnetapp
+# dotnet publish -c Release -o out
+# ./bin/container/Release/net8.0/aspnetapp
