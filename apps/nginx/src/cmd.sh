12d10cd79d1a7a3cbc43699883e51958ad47dca3
Darren <27513732@qq.com>
2024-05-29 13:44:38 +0800

    add demo folder

--- apps/nginx/src/cmd.sh
+++ apps/nginx/src/cmd.sh
@@ -1,9 +1,16 @@
-echo "Add your CI code here, below is example"
 
+### Default Demo which you can delete it
+
+echo "Add your CI code here, below is example"
 echo "Hello, Nginx!" > /usr/share/nginx/html/index.html
+
+### Install packages that you may use for deployment
 
 # apt update -y
 # apt install -y wget unzip
-# wget https://qiaofeng1227.github.io/html.zip -P /usr/share/nginx/html
-# rm -rf /usr/share/nginx/html/index.html
-# unzip /usr/share/nginx/html/html.zip -d /usr/share/nginx/html
+
+
+### Sample for reference
+# rm -rf /usr/share/nginx/html/*
+# wget https://qiaofeng1227.github.io/demo.zip -P /usr/share/nginx/html
+# unzip /usr/share/nginx/html/demo.zip -d /usr/share/nginx/html
