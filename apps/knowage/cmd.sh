2ca229943be2857fffae9d5a6afbe0746c0b2b18
qiaofeng1227 <76487013@qq.com>
2024-07-19 17:07:36 +0800

    knowage

--- apps/knowage/cmd.sh
+++
@@ -1,5 +0,0 @@
-sed -i "s/password=\"biadmin\"/password=\"$W9_LOGIN_PASSWORD\"/g" /home/knowage/apache-tomcat/webapps/knowage/WEB-INF/conf/config/internal_profiling.xml
-sed -i "s/password=\"biadmin\"/password=\"$W9_LOGIN_PASSWORD\"/g" /home/knowage/apache-tomcat/webapps/knowage/WEB-INF/conf/webapp/authorizations.xml
-mysqladmin -uroot -p$DB_PASS -h 127.0.0.1 knowage -e "DELETE FROM knowage.SBI_USER;"
-
-./entrypoint.sh ./apache-tomcat/bin/startup.sh
