3c93a261944570638b738e3230162f64a222015a
qiaofeng1227 <76487013@qq.com>
2024-11-19 16:19:05 +0800

    mediawiki

--- apps/nodejs/src/cmd.sh
+++ apps/nodejs/src/cmd.sh
@@ -1,5 +1,22 @@
 echo "Add your CI code here, below is example"
 
+cat > app.js <<EOF
+const http = require('http');
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('Hello World!');
+    res.end();
+});
+
+server.listen(port, () => {
+    console.log(\`Example app listening on port \${port}\`);
+});
+EOF
+
+node app.js
+
 ## Sample One: Express
 
 # npm install express --save
