06fe4aa9f0f3341b781491e95dd7369feba36c52
Darren <27513732@qq.com>
2024-11-19 18:25:29 +0800

    Update cmd.sh

--- apps/nginx/src/cmd.sh
+++ apps/nginx/src/cmd.sh
@@ -1,15 +1,22 @@
 ### Default Demo which you can delete it
 
 echo "Add your CI code here, below is example"
-echo "Hello, Nginx!" > /usr/share/nginx/html/index.html
+if [! -e /usr/share/nginx/html/index.html ]; then
+    echo "Hello, Nginx!" > /usr/share/nginx/html/index.html
+fi
 
 ### Install packages that you may use for deployment
 
 # apt update -y
 # apt install -y wget unzip
 
-
 ### Sample for reference
 # rm -rf /usr/share/nginx/html/*
 # wget https://websoft9.github.io/docker-library/apps/nginx/demo/demo.zip -P /usr/share/nginx/html
 # unzip /usr/share/nginx/html/demo.zip -d /usr/share/nginx/html
+
+
+
+
+### Fix upload files permission, it very important, don't delete
+chown -R nginx:nginx /usr/share/nginx/html
