8e3e0b2ac4261b28eac8c1ef82d56e2e4dd80f7f
qiaofeng1227 <76487013@qq.com>
2025-04-21 17:07:48 +0800

    magento

--- apps/magento/src/cmd.sh
+++ apps/magento/src/cmd.sh
@@ -1,22 +1,32 @@
 ## This script is always excused after PHP container running
 ## If you upload your PHP application source code to container, you should consider migration exist data
 
-## Sample for you
-
 if [ -z "$(ls -A /var/www/html)" ]; then
   echo "<?php phpinfo(); ?>" > /var/www/html/index.php
   chown -R www-data:www-data /var/www/html
   echo "Commands executed: index.php created and ownership changed."
 else
-  echo "/var/www/html is not empty. No actions taken."
-  chown -R www-data:www-data /var/www/html
+  echo "Start to create magento site..."
+  bin/magento setup:install \
+  --base-url=http://${W9_URL} \
+  --db-host=${W9_ID}-mariadb \
+  --db-name=magento \
+  --db-user=magento \
+  --db-password=${W9_RCODE} \
+  --backend-frontname=admin  \
+  --admin-firstname=admin \
+  --admin-lastname=admin \
+  --admin-email=admin@admin.com \
+  --admin-user=admin \
+  --admin-password=${W9_RCODE} \
+  --language=en_US \
+  --currency=USD \
+  --timezone=America/Chicago \
+  --use-rewrites=1 \
+  --search-engine=opensearch \
+  --opensearch-host=${W9_ID}-opensearch \
+  --opensearch-port=9200 \
+  --opensearch-index-prefix=magento2 \
+  --opensearch-timeout=15 \
+  --disable-modules=Magento_TwoFactorAuth 
 fi
-
-
-## Install WordPress for your reference, you should improve it for migration exist data
-
-# cd /var/www/html 
-# curl -O https://wordpress.org/latest.zip
-# unzip latest.zip
-# mv wordpress/* ./
-# chown -R www-data:www-data /var/www/html
