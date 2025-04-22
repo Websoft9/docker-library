d7d76ad3f69e52367eeefe1975d55230d7faae18
qiaofeng1227 <76487013@qq.com>
2025-04-22 15:47:12 +0800

    magento

--- apps/magento/src/cmd.sh
+++ apps/magento/src/cmd.sh
@@ -1,13 +1,36 @@
 ## This script is always excused after PHP container running
 ## If you upload your PHP application source code to container, you should consider migration exist data
 
+# Check MySQL connection
+check_mysql() {
+  echo "Verifying MySQL connection..."
+  mysql -h "${W9_ID}-mariadb" -u magento -p"${W9_RCODE}" -e 'SELECT 1' magento &>/dev/null
+  return $?
+}
+
+# Check OpenSearch health
+check_opensearch() {
+  echo "Verifying OpenSearch health..."
+  curl -sSf http://${W9_ID}-opensearch:9200/_cluster/health?pretty | grep -q '"status" : "green"'
+  return $?
+}
+  
 if [ -f "/var/www/html/vendor/magento/language-zh_hans_cn/zh_CN.csv" ]; then
   echo "magento site already created, skip it"
   chown -R www-data:www-data /var/www/html
 else
 
   echo "Start to create magento site..."
-  sleep 30
+
+  # DB and OpenSearch must ready before create site
+  until check_mysql; do
+    sleep 5
+  done
+  
+  until check_opensearch; do
+    sleep 5
+  done
+
   # magento create site
   bin/magento setup:install \
   --base-url=http://${W9_URL} \
